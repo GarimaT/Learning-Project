f3455cfacb5849c849249c558076e2593440e88a
Garima Taneja <garima.taneja@berylsystems.com>
2017-05-31 18:06:27 +0530

    Edit and update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,12 @@
 class ArticlesController < ApplicationController
 
-	def new
+	def new 
 		@article = Article.new
 		
+	end
+
+	def edit
+		@article = Article.find(params[:id])
 	end
 
 	def create
@@ -17,6 +21,17 @@ class ArticlesController < ApplicationController
 		end
 		#@article.save
 		#redirect_to articles_path(@article)
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+
 	end
 
 	def show
