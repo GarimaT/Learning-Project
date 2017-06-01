dcef36be7d6a48c1f8631ad3cbddb7e48d2f3ec6
Garima Taneja <garima.taneja@berylsystems.com>
2017-06-01 17:13:42 +0530

    changes in application controller, for code redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 	
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
+
 	def index
 		@article = Article.all
 	end
@@ -10,7 +12,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
+		
 	end
 
 	def create
@@ -28,7 +30,6 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-		@article = Article.find(params[:id])
 		if @article.update(article_params)
 			flash[:notice] = "Article was successfully updated"
 			redirect_to article_path(@article)
@@ -39,13 +40,11 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
-		@article = Article.find(params[:id])
-
+		
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
-		@article.destroy
+		@article.destroy 
 		flash[:notice] = "Article deleted successfully"
 		redirect_to articles_path
 	end
@@ -59,8 +58,8 @@ class ArticlesController < ApplicationController
 
 	  end
 
-
-
-
+	  def set_article
+	  	@article = Article.find(params[:id])
+	  end
 
 end
